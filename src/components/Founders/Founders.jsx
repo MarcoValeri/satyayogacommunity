5c2e4842806e91350e5ea124a1251f0588256ad3
MarcoValeri <marcovaleri@hotmail.it>
2024-07-27 17:59:27 +0100

    Add founders component

--- src/components/Founders/Founders.jsx
+++ src/components/Founders/Founders.jsx
@@ -5,17 +5,21 @@ import SatyaYogaFounders from "../../images/satya-yoga-founders.webp";
 const Founders = () => {
     return (
         <div className="founders">
-            <div className="founders__container-white-small"></div>
-            <div className="founders__container-pink"></div>
-            <div className="founders__container-title">
-                <h2 className="founders__title h1">FOUNDERS</h2>
-            </div>
-            <div className="founders__container-img">
-                <img className="founders__img" src={SatyaYogaFounders} alt="Collage with the founders of Satya yoga, Caterina and Valentina, both in a park while are doing yoga" />
-            </div>
+            <div className="founders__container-background-pink"></div>
+            <div className="founders__container-background-white"></div>
             <div className="founders__container-content">
-                <p className="p-sm">Satya Yoga was co-founded by the triyoga graduates Caterina Giordo and Vittoria Vergani.</p>
-                <p className="p-sm">Strong advocates for the yoga community, together, they created a supportive space where yogis can connect, grow, and thrive.</p>
+                <div className="founders__container-content-title">
+                    <h2 className="founders__content-title h1">FOUNDERS</h2>
+                </div>
+                <div className="founders__container-content-info">
+                    <div className="founders__container-content-info-image">
+                        <img className="founders__content-image" src={SatyaYogaFounders} alt="Caterina and Vittoria, the two founders that are making yoga at the park" />
+                    </div>
+                    <div className="founders__container-content-info-card">
+                        <p className="founders__container-content-info-card-p p-sm">Satya Yoga was co-founded by the triyoga graduates Caterina Giordo and Vittoria Vergani.</p>
+                        <p className="p-sm">Strong advocates for the yoga community, together, they created a supportive space where yogis can connect, grow, and thrive.</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
                     />
                 </div>
             </main>
-            {/* <Founders /> */}
+            <Founders />
         </div>
     )
 }
