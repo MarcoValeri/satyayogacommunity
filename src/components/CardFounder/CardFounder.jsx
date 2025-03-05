3049558e0f00899d62640f40e3f5dfeef713272f
MarcoValeri <marcovaleri@hotmail.it>
2025-03-05 23:36:07 +0000

    Add founder card

---
+++ src/components/CardFounder/CardFounder.jsx
@@ -0,0 +1,33 @@
+import { FaInstagram } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
+
+import "./CardFounder.scss";
+
+const CardFounder = (props) => {
+
+    return (
+        <div className="card-founder">
+            <div className="card-founder__wrapper">
+                <div className="card-founder__container-image">
+                    <img className="card-founder__image" src={props.founderImage} alt="Founder members of the Satya Yoga" />
+                </div>
+                <div className="card-founder__container-data">
+                    <div className="card-founder__container-data-close">
+                        <IoCloseSharp
+                            className="card-founder__close-icon"
+                            onClick={props.onCloseCard}
+                        />
+                    </div>
+                    <div className="card-founder__container-data-name">
+                        <h2 className="card-founder__name h2">{props.founderName}</h2>
+                        <h3 className="card-founder__title h4">{props.founderTitle}</h3>
+                    </div>
+                    <p className="card-founder__quote p-lg">{props.founderQuote}</p>
+                    <a className="card-founder__instagram" href={props.founterInstagram} target="_blank"><FaInstagram /></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardFounder;
--- src/components/Founders/Founders.jsx
+++ src/components/Founders/Founders.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import CardFounder from "../CardFounder/CardFounder";
+
 import "./Founders.scss";
 
 import SatyaYogaFounders from "../../images/vittoria-caterina-satya-yoga-founders.webp";
 
 const Founders = () => {
+
+    const [showCaterinaCard, setShowCaterinaCard] = useState(false);
+
+    const handleCaterinaCard = () => {
+        showCaterinaCard ? setShowCaterinaCard(false) : setShowCaterinaCard(true);
+    }
+
     return (
         <div className="founders">
             <div className="founders__container-background-pink"></div>
@@ -16,11 +26,26 @@ const Founders = () => {
                         <img className="founders__content-image" src={SatyaYogaFounders} alt="Caterina and Vittoria, the two founders that are making yoga at the park" />
                     </div>
                     <div className="founders__container-content-info-card">
-                        <p className="founders__container-content-info-card-p p-sm">Satya Yoga was co-founded by triyoga graduates<br/><a className="link-red" href="https://www.instagram.com/catgiordo/" target="_blank">Caterina Giordo</a> and <a className="link-red" href="https://www.instagram.com/vickivergani/"  target="_blank">Vittoria Vergani</a>.</p>
+                        {/* <p className="founders__container-content-info-card-p p-sm">Satya Yoga was co-founded by triyoga graduates<br/><a className="link-red" href="https://www.instagram.com/catgiordo/" target="_blank">Caterina Giordo</a> and <a className="link-red" href="https://www.instagram.com/vickivergani/"  target="_blank">Vittoria Vergani</a>.</p> */}
+                        <p className="founders__container-content-info-card-p p-sm">Satya Yoga was co-founded by triyoga graduates<br/><span className="link-red" onClick={handleCaterinaCard}>Caterina Giordo</span> and <a className="link-red" href="https://www.instagram.com/vickivergani/"  target="_blank">Vittoria Vergani</a>.</p>
                         <p className="p-sm">Strong advocates for the yoga community, together, they created a supportive space where yogis can connect, grow, and thrive.</p>
                     </div>
                 </div>
             </div>
+            {showCaterinaCard && 
+                (
+                <div className="founders__container-cards">
+                    <CardFounder
+                        founderImage={SatyaYogaFounders}
+                        onCloseCard={handleCaterinaCard}
+                        founderName="Caterina Giordo"
+                        founderTitle="Yoga Instructor"
+                        founderQuote="Inner peace outer strength"
+                        founterInstagram="https://www.instagram.com/catgiordo/"
+                    />
+                </div>
+                )
+            }
         </div>
     )
 }
